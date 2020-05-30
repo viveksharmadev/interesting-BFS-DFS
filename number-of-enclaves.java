4e840227e2144f56088b2371409498a5a45ac9f3
Learner <vivek.gurukul1982@gmail.com>
2020-05-30 12:01:06 -0700

    Update number-of-enclaves.java

--- number-of-enclaves.java
+++ number-of-enclaves.java
@@ -48,4 +48,44 @@ class number-of-enclaves {
         dfs(row, col+1, A);
         dfs(row, col-1, A);
     }
+    
+    // Simpler
+    // tc -> m*n, sc-> m*n
+    public int numEnclaves(int[][] A) {
+        int res = 0;
+        if(A==null || A.length==0) return res;
+        
+        int m = A.length, n = A[0].length;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(A[i][j]==1){
+                    if(i==0 || j==0 || i==m-1 || j==n-1){
+                        dfs(A, i, j);
+                    }
+                }   
+            }            
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(A[i][j]==1){
+                    res++;
+                }
+            }            
+        }
+        
+        return res;
+    }
+    
+    private void dfs(int[][] A, int row, int col){
+        if(row<0 || row>A.length-1 || col<0 || col>A[0].length-1 
+           || A[row][col]!=1)
+            return;
+        A[row][col] = 0;
+        dfs(A, row+1, col);
+        dfs(A, row-1, col);
+        dfs(A, row, col+1);
+        dfs(A, row, col-1);
+    }
 }
