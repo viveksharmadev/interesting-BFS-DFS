8eae23a1b46a1f0ad5cb089522e55fb82f21d246
Learner <vivek.gurukul1982@gmail.com>
2020-06-11 11:48:46 -0700

    Create partition-to-k-equal-sum-subsets.java

---
+++ partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+// Same as https://leetcode.com/problems/matchsticks-to-square/
+class partition-to-k-equal-sum-subsets {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        if(nums==null || nums.length==0){
+            return false;
+        }
+        int sum = 0;
+        for(int num : nums){
+            sum+=num;
+        }
+        if(k<=0 || sum%k!=0){
+            return false;
+        }
+        
+        boolean[] visited = new boolean[nums.length];
+        return helper(nums, visited, k, 0, 0, sum/k);
+    }
+    
+    private boolean helper(int[] nums, boolean[] visited, int k, int startIndex, int currSum, int target){
+        if(currSum > target){
+            return false;
+        }
+        if(k==1){
+            return true;
+        }
+        
+        if(currSum == target){
+            return helper(nums, visited, k-1, 0, 0, target);
+        }
+        
+        for(int i=startIndex; i<nums.length; i++){
+            if(!visited[i]){
+                visited[i]=true;
+            if(helper(nums, visited, k, i+1, currSum+nums[i], target)){
+                return true;
+            }
+                visited[i]=false;
+        }
+    }
+        return false;
+}
+}
