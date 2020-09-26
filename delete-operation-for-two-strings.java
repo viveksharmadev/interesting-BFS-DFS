12d89bbee512b6966997510c46e909fc2fe87358
Learner <vivek.gurukul1982@gmail.com>
2020-09-26 14:38:02 -0700

    Update delete-operation-for-two-strings.java

--- delete-operation-for-two-strings.java
+++ delete-operation-for-two-strings.java
@@ -59,3 +59,35 @@ class Solution {
         return cache[i][j];
     }
 }
+
+// Without Recursion
+class Solution {
+    // tc -> m*n, sc-> m*n
+    public int minDistance(String word1, String word2) {
+        if(word1==null && word2==null)
+            return 0;
+        
+        int m = word1.length(), n = word2.length();
+        
+        int lcs = getLCS(word1, word2, m, n, new int[m+1][n+1]);
+        
+        return (m+n) - (2*lcs);
+    }
+    
+    
+    private int getLCS(String word1, String word2, int m, int n, int[][] cache){
+      
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    cache[i][j] = cache[i-1][j-1] + 1;
+                }else{
+                    cache[i][j] = Math.max(cache[i-1][j], cache[i][j-1]);
+                }
+            }
+        }
+        
+        return cache[m][n];
+    }
+    
+}
